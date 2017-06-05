aa3ee41cae1c118f88052dd9e1518cee7e48ab22
Michael Megarbane <MJ@Monas-MacBook.fios-router.home>
2017-06-05 12:09:38 -0400

    started the layout of game

---
+++ assets/javascript/game.js
@@ -0,0 +1,67 @@
+var characters = {
+
+  darthMaul: {
+  hitpoints:"100",
+  attackPwr:"20",
+  counteratkPwr:"5",
+  picture:"../../images/Darth-Maul.jpeg",
+  name: "Darth Maul",
+},
+
+
+  ObiWanKenobi: {
+    hitpoints:"100",
+    attackPwr:"20",
+    counteratkPwr:"5",
+  },
+
+
+
+  DarthVader:{
+    hitpoints:"100",
+    attackPwr:"20",
+    counteratkPwr:"5",
+},
+
+  QuiGonJinn:{
+    hitpoints:"100",
+    attackPwr:"20",
+    counteratkPwr:"5",
+},
+
+}
+
+
+var testArray= []
+characterArray = [["darthMaul","ObiWanKenobi","DarthVader","QuiGonJinn"],["assets/images/Darth-Maul.jpeg","assets/images/Obi-Wan.jpg", "assets/images/Darth-Sidious.png", "assets/images/Qui Gon Jinn.jpeg"]]
+
+
+
+
+for(i=0; i<4; i++)
+{
+  var newimg = $("<img>");
+  newimg.attr("src", (characterArray[1][i]));
+  newimg.attr("class", "characterImg");
+  newimg.attr("value" , (characterArray[0][i]))
+  $("#characterstochoose").append(newimg);
+  testArray.push(characterArray[0][i]);
+
+}
+
+
+
+
+$(document).on("click","img",function(){
+
+if($(this).hasClass("characterImg"))
+
+{
+
+  testArray.splice(testArray.indexOf($(this).attr("value")),1)
+  console.log($(this).attr("value"))
+}
+
+
+
+});
