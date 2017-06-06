197931ac114cbb1fb66359930c0f4ba7ea3d8019
Michael Megarbane <MJ@Monas-MacBook.fios-router.home>
2017-06-06 10:32:24 -0400

    working on setting game up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,6 @@ var characters = {
   hitpoints:"100",
   attackPwr:"20",
   counteratkPwr:"5",
-  picture:"../../images/Darth-Maul.jpeg",
   name: "Darth Maul",
 },
 
@@ -33,8 +32,9 @@ var characters = {
 
 
 var testArray= []
-characterArray = [["darthMaul","ObiWanKenobi","DarthVader","QuiGonJinn"],["assets/images/Darth-Maul.jpeg","assets/images/Obi-Wan.jpg", "assets/images/Darth-Sidious.png", "assets/images/Qui Gon Jinn.jpeg"]]
-
+var testArray2= []
+characterArray = [["darthMaul","ObiWanKenobi","DarthSidious","QuiGonJinn"],["assets/images/Darth-Maul.jpeg","assets/images/Obi-Wan.jpg", "assets/images/Darth-Sidious.png", "assets/images/Qui Gon Jinn.jpeg"]]
+EnemiesArry = [];
 
 
 
@@ -46,10 +46,9 @@ for(i=0; i<characterArray[0].length; i++)
   newimg.attr("value" , (characterArray[0][i]))
   $("#characterstochoose").append(newimg);
   testArray.push(characterArray[0][i]);
+  testArray2.push(characterArray[1][i]);
 
 }
-
-
 
 
 $(document).on("click","img",function(){
@@ -57,32 +56,30 @@ $(document).on("click","img",function(){
 if($(this).hasClass("characterImg"))
 
 {
+  $("#characterstochoose").empty();
+
+  $("#characterstochoose").append($(this))
 
   testArray.splice(testArray.indexOf($(this).attr("value")),1)
+
 
   console.log($(this).attr("value"))
 }
 
-
-
-
-for(j=0;j<characterArray.length;j++)
+for (j=0; j<testArray.length;j++)
 
 {
   var newimg = $("<img>");
-
-  for(k=0;k<testArray.length;k++)
-  {
-  if(characterArray[j] === testArray[k] )
-
-
-  newimg.attr("src", (characterArray[1][j]));
-  newimg.attr("class", "enemiesImg");
-  newimg.attr("value" , (characterArray[0][j]));
+  newimg.attr("src", (testArray2[j]));
+  newimg.attr("class", "characterImg");
+  newimg.attr("value" , (characterArray[j]))
   $("#enemies").append(newimg);
 
+
 }
 
 
-}
+
+
+
 });
