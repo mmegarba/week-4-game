76614a2f6447a55ba520fef5e286504b8b2b88b1
Michael Megarbane <MJ@Monas-MacBook.fios-router.home>
2017-06-07 15:20:56 -0400

    game logic started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,17 +12,17 @@ var characters = [
   {
     name:"ObiWanKenobi",
     hitpoints:"100",
-    attackPwr:"20",
-    counteratkPwr:"5",
+    attackPwr:"10",
+    counteratkPwr:"10",
     img:"assets/images/Obi-Wan.jpg",
   },
 
 
 
 {
-    name:"DarthVader",
+    name:"DarthSidious",
     hitpoints:"200",
-    attackPwr:"20",
+    attackPwr:"5",
     counteratkPwr:"5",
     img:"assets/images/Darth-Sidious.png",
 },
@@ -30,23 +30,28 @@ var characters = [
 {
     name:"QuiGonJinn",
     hitpoints:"125",
-    attackPwr:"20",
+    attackPwr:"7",
     counteratkPwr:"5",
     img:"assets/images/Qui Gon Jinn.jpeg",
 },
 
 ]
 
-//
-// var testArray= []
-// var testArray2= []
-// characterArray = [["darthMaul","ObiWanKenobi","DarthSidious","QuiGonJinn"],["assets/images/Darth-Maul.jpeg","assets/images/Obi-Wan.jpg", "assets/images/Darth-Sidious.png", "assets/images/Qui Gon Jinn.jpeg"]]
-// EnemiesArry = [];
+var currentAttacker;
+var currentDefender;
+var currentD;
+var currentA;
 
 
 
 
+function updateStats(){
 
+console.log(parseInt(characters[currentA].hitpoints))
+console.log(parseInt(characters[currentD].hitpoints))
+
+
+}
 
 
 
@@ -63,6 +68,7 @@ for(i=0; i<characters.length; i++)
   newspanName.html(characters[i].name)
 
   newimg.attr("src", (characters[i].img));
+  newimg.attr("value", (characters[i].name))
   newimg.attr("class", "characterImg");
   // newimg.attr("value" , (characterArray[0][i]))
   $("#characterstochoose").append(newdiv);
@@ -76,35 +82,118 @@ for(i=0; i<characters.length; i++)
 }
 //
 //
-// $(document).on("click","img",function(){
+
+
+$("#enemies").on("click","img",function(){
+
+  // for picking a defender
+
+  if($(this).hasClass("enemiesImg"))
+  {
+
+    for(j=0; j<characters.length;j++)
+
+    {
+      if(characters[j].name === $(this).attr("value"))
+
+      {
+        var enemyImg = $("<img>");
+        enemyImg.attr("src", (characters[j].img));
+        enemyImg.attr("value", (characters[j].name))
+        enemyImg.attr("class", "characterImg");
+        enemyImg.attr("data", "defender")
+        $("#Defender").append(enemyImg)
+
+        currentDefender = characters[j].name;
+        currentD = j
+
+
+      }
+
+    }
+
+  }
+
+  });
+
+
+
+
+
+
+$("#characterstochoose").on("click","img",function(){
+
+// for picking a attacker
+  if($(this).hasClass("characterImg"))
+{
+
+
+  for(i=0; i<characters.length; i++)
+{
+  if(characters[i].name != $(this).attr("value") )
+
+  {
+    var newimg = $("<img>");
+    newimg.attr("src", (characters[i].img));
+    newimg.attr("value", (characters[i].name))
+    newimg.attr("class", "enemiesImg");
+    $("#enemies").append(newimg);
+
+
+  }
+
+  else{
+    $("#characterstochoose").empty()
+    var newimg = $("<img>");
+    newimg.attr("src", (characters[i].img));
+    newimg.attr("value", (characters[i].name))
+    newimg.attr("class", "characterImg");
+    $("#characterstochoose").append(newimg)
+
+    currentAttacker = characters[i].name;
+    currentA = i;
+  }
+}
+
+}
+});
+
+
+$("#attack").on("click", function(){
+
+// check to see if hitpoints not below zero for either attacker or defender
+
+// if()
+
+
+$(".characterImg").each(function()
+
+{
+  if($(this).attr("value") === currentDefender)
+{
+    characters[currentD].hitpoints = parseInt(characters[currentD].hitpoints)  -  parseInt(characters[currentA].attackPwr)
+    console.log(currentDefender)
+  }
+
+  if($(this).attr("value") === currentAttacker)
+
+
+  {
+    characters[currentA].hitpoints = parseInt(characters[currentA].hitpoints) - parseInt(characters[currentD].counteratkPwr)
+    console.log(currentAttacker)
+
+  }
+
+});
+
+
+
+updateStats()
+
+
+
+});
+
+
+
 //
-// if($(this).hasClass("characterImg"))
-//
-// {
-//   $("#characterstochoose").empty();
-//
-//   $("#characterstochoose").append($(this))
-//
-//   testArray.splice(testArray.indexOf($(this).attr("value")),1)
-//
-//
-//   console.log($(this).attr("value"))
-// }
-//
-// for (j=0; j<testArray.length;j++)
-//
-// {
-//   var newimg = $("<img>");
-//   newimg.attr("src", (testArray2[j]));
-//   newimg.attr("class", "characterImg");
-//   newimg.attr("value" , (characterArray[j]))
-//   $("#enemies").append(newimg);
-//
-//
-// }
-//
-//
-//
-//
-//
-// });
